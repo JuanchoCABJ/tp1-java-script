63d6eaa84244b74930d00397f062ed0ab8d66142
JuanchoCABJ <trusjuanmanuel@gmail.com>
2026-04-10 21:43:08 -0300

    archivos

---
+++ Proyecto 1/Js/app.js
@@ -0,0 +1,10 @@
+//Nombre y Apellido: Juan Trus
+//Curso: 6B
+//Grupo: 604
+//Taller de programacion III
+//Departamento de Informatica - EESTn°1 - Raul Scalabrini Ortiz
+
+const nuevaString = string ;
+const nuevoNum = 7;
+const nuevoBool = true; 
+const nuevaResta = 10 - 5 === 5;
