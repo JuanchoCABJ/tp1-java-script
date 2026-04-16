0cfd0a384f607644c4151b7031c16e9593fd6f17
JuanchoCABJ <trusjuanmanuel@gmail.com>
2026-04-16 20:41:24 -0300

    agregue mas cosas

--- Proyecto 1/Js/app.js
+++ Proyecto 1/Js/app.js
@@ -4,9 +4,23 @@
 //Taller de programacion III
 //Departamento de Informatica - EESTn°1 - Raul Scalabrini Ortiz
 
-const nuevaString = Juan;
+const nuevaString = "Juan";
 const nuevoNum = 7;
 const nuevoBool = true; 
 const nuevaResta = 10 - 5 === 5;
 const nuevaMultiplicacion = 10 * 4 === 40 ;
 const nuevoModulo = 21 % 5 === 1;
+
+function devolverString(str) {
+    const nuevaString = "Juan";
+return (str);
+}
+
+function suma(x, y) {
+  return x + y;
+}
+
+function resta(x, y) {
+  return x-y;
+}
+
